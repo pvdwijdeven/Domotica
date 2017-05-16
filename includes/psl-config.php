9a17d40f7010a388b136898d11c4006bd171b822
pvdwijdeven <pvdwijdeven@gmail.com>
2017-05-16 13:33:54 +0200

    Update psl-config.php
    Added measurement database

--- includes/psl-config.php
+++ includes/psl-config.php
@@ -5,7 +5,8 @@
 define("HOST", "localhost");     // The host you want to connect to.
 define("USER", "sec_user");    // The database username.
 define("PASSWORD", "ImNotThatStupid");    // The database password.
-define("DATABASE", "secure_login");    // The database name.
+define("DATABASE", "secure_login");    // The database name for config.
+define("DATABASE_MEAS", "measurements");    // The database name for measurements.
 
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "member");
