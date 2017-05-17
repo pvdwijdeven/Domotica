980649cbd78235ef7b6b9c6f6d18ef49e835827f
pvdwijdeven <pvdwijdeven@gmail.com>
2017-05-17 16:51:07 +0200

    User login fixed

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'psl-config.php';   // As functions.php is not included
+include_once 'includes/psl-config.php';   // As functions.php is not included
 $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 mysql_connect(HOST,USER,PASSWORD);
 	mysql_select_db("measurements");
