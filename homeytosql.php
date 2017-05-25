95674f61ac2403ed0e7da6eddefc8fd224f4ce89
pvdwijdeven <pvdwijdeven@gmail.com>
2017-05-26 01:20:01 +0200

    Removed tiny error in duplicate SQL

--- homeytosql.php
+++ homeytosql.php
@@ -10,7 +10,7 @@ if ($val=="true") {$val=1;}
 if ($val=="false") {$val=0;}
 
 
-$sqlstring="INSERT INTO measurement_values (" . $fields . ") VALUES (" . $epoch . ", ". $val . ") ON DUPLICATE KEY UPDATE val".$_GET['measurement']."=val".$_GET['measurement']; 
+$sqlstring="INSERT INTO measurement_values (" . $fields . ") VALUES (" . $epoch . ", ". $val . ") ON DUPLICATE KEY UPDATE val".$_GET['measurement']."=".$val; 
 file_put_contents("log/logfile.txt", "homeytosql.php ".$sqlstring."\n", FILE_APPEND | LOCK_EX);
 
 $result = mysql_query($sqlstring) or die('something went wrong with '.mysql_error(). ' sql-string: '.$sqlstring);
