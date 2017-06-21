5549214016a0333e77d0a7e2164643418b3b0d44
pvdwijdeven <pvdwijdeven@gmail.com>
2017-06-21 17:42:29 +0200

    Added autokill for getcurrentvalues to prevent too many mysql connections Updated internal IP -> why did it change?

--- getCurrentValues.php
+++ getCurrentValues.php
@@ -3,7 +3,7 @@
 	unlink($filename);
 	
 	include_once 'includes/db_connect.php';
-	$api_url = 'http://192.168.1.64:8000/api/app/com.internet/getCurrent';
+	$api_url = 'http://192.168.1.67:8000/api/app/com.internet/getCurrent';
 	$sql = "SELECT * FROM config where row= 'config'";
 	$result = $mysqli->query($sql);
 	$row = $result->fetch_assoc();
@@ -19,9 +19,16 @@
 		$result = file_get_contents($api_url, false, $context);
 		if ($result===FALSE)
 		{echo "Unauthorized request";}
-		while (!file_exists($filename)){sleep(1);}
-		echo date ("Y/m/d H:i:s", filemtime($filename));
-		$myfile = fopen($filename, "r") or die("Unable to open file!");
-		echo strstr(ltrim(strstr(fread($myfile,filesize($filename)), ','),','),',');
-		fclose($myfile);
+		$maxtime=4;
+		$curtime=0;
+		while (!file_exists($filename) and $curtime<$maxtime){
+			sleep(1);
+			$curtime++;
+		}
+		if (file_exists($filename)){
+			echo date ("Y/m/d H:i:s", filemtime($filename));
+			$myfile = fopen($filename, "r") or die("Unable to open file!");
+			echo strstr(ltrim(strstr(fread($myfile,filesize($filename)), ','),','),',');
+			fclose($myfile);
+		}
 ?>
